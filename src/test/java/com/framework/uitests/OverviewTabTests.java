aaed5e2e8428432bed897d5e2fcd0a70e2d241d0
sakkaya <sakkaya@simlytest.de>
2021-12-28 00:09:05 +0100

    OverviewTabTests added and refactored

---
+++ src/test/java/com/framework/uitests/OverviewTabTests.java
@@ -0,0 +1,49 @@
+package com.framework.uitests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OverviewTabTests extends BaseTestClass{
+
+    //Arrange
+    String user = "andrejs-ps";
+
+    @BeforeEach
+    void overviewTabSetUp() {
+        // open the browser at a specific URL - https://github.com/andrejs-ps
+        driver.get(BASE_URL + user);
+    }
+
+    @AfterEach
+    void localCleanUp() {
+        // nothing to do here now, but maybe in the future?
+    }
+
+    @Test
+    void userNameIsCorrectOnOverviewTab() {
+        //Act
+        String actualUserName = driver.findElement(By.className("p-nickname")).getText();
+        //Assert
+        assertEquals(user, actualUserName);
+    }
+
+    @Test
+    void repoLinkGoesToCorrectRepo() {
+        //Act
+        String repo = "automated-tests-in-java-with-fluent-interface-using-webdriver-selenium";
+        WebElement repoLink = driver.findElement(By.linkText(repo));
+        repoLink.click();
+        String actualUrl = driver.getCurrentUrl();
+        //Assert
+        assertEquals(BASE_URL + "andrejs-ps/" + repo , actualUrl);
+
+    }
+
+
+
+
+}
